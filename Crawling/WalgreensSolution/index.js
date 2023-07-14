81bd05dc2763acd5d7406053ca90140e6acda77b
soggylithe <nivek1892@gmail.com>
2023-07-14 18:46:23 -0500

    Fix stubborn scroll and convert to headless

--- Crawling/WalgreensSolution/index.js
+++ Crawling/WalgreensSolution/index.js
@@ -2,23 +2,26 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async function(){
-    const browser = await puppeteer.launch({ headless: false});
+    const browser = await puppeteer.launch({ headless: true});
     const page = await browser.newPage();
     const baseURL = "http://www.Walgreens.com";
 
     page.goto(baseURL);
     await page.waitForNavigation();
     await setupJquerry(page);
-    
-    let householdScraper = new CategoryScraper(page, "Household & Pet Essentials");
-    await householdScraper.scrape(1);
+    console.log("Page set up.");
 
+    let householdScraper = new CategoryScraper(page, "Household & Pet Essentials");
+    await householdScraper.scrape(10);
+    await page.close();
+    
     // let otherPage = await browser.newPage();
     // await otherPage.goto(baseURL);
     // let beautyScraper = new CategoryScraper(otherPage, "Beauty");
     // await beautyScraper.scrape(1);
 
-    fs.writeFile(`./${householdScraper.category}_Data.json`, JSON.stringify({products: householdScraper.outData},undefined,4), err =>{console.log(err|null)});
+    console.log("Writing data to file");
+    fs.writeFile(`./${householdScraper.category}_Data.json`, JSON.stringify({products: householdScraper.outData},undefined,4), err =>{if(err) console.log(err|null)});
 })();
 
 async function setupJquerry(page){
@@ -37,6 +40,7 @@ class CategoryScraper{
     }
 
     async scrape(count){
+        console.log("Beginning scrape.");
         await this.navigateToProductList();
         for (let i=1; i<=count;i++){
             //click into a product
@@ -53,18 +57,23 @@ class CategoryScraper{
     }
 
     async navigateToProductList(){
-
+        console.log('Navigating to shop page.');
         await this.page.locator('a.menu-trigger').click();
         await this.page.locator('#menu-shop-products> a').click();
-        await this.page.locator(`#menu-shop-products li> a[data-element-name="${this.category}"]`).click();
+        await Promise.all( [this.page.locator(`#menu-shop-products li> a[data-element-name="${this.category}"]`).click(),
+                            new Promise(r=> setTimeout(r,2000))]);
+        await this.page.evaluate(function(){
+            $('.default-dropdown.menu-dropdown.show').scrollTop(500);
+        });
+        //Element has trouble being scrolled to, instead is directly handled by JQuery
         await this.page.locator(`#menu-shop-products li> .show-next-lvl .right-links li> a[data-element-name="Shop ${this.category}"]`).click();
         
-        //TODO:Need to scroll to element, not doing it by default
         await this.page.locator('#category_Link>li:last-of-type>a').click();
         await this.page.waitForNavigation();
     }
 
     async scrapeProductPage(){
+        console.log(`Scraping ${this.category} page.`);
         let pageJSON = await this.page.evaluate(function(){
             let productName = $('#productTitle')[0].innerText;
             

09aebdde8f2fb5351f79839d90cdf36244ae54eb
soggylithe <nivek1892@gmail.com>
2023-07-14 11:37:34 -0500

    Refactor common logic into container class
    CategoryScraper handles finding the relevant shop page and pulling data from product pages inside.

--- Crawling/WalgreensSolution/index.js
+++ Crawling/WalgreensSolution/index.js
@@ -1,35 +1,16 @@
 const puppeteer = require('puppeteer');
-const ReturnData = require('./datatemplate.js');
+//const ReturnData = require('./datatemplate.js');
 
 (async function(){
     const browser = await puppeteer.launch({ headless: false});
-    
     const page = await browser.newPage();
     const baseURL = "http://www.Walgreens.com";
 
-    let navigate = async function (category){
-        page.goto(baseURL);
-        await page.waitForNavigation();
-        await setupJquerry(page);
-        //await page.addScriptTag({url: "https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"});
-
-        await page.locator('a.menu-trigger').click();
-        await page.locator('#menu-shop-products>a').click();
-        await page.locator('#menu-shop-products li>a[data-element-name="Household & Pet Essentials"]').click();
-        await page.locator('#menu-shop-products li>.show-next-lvl .right-links li>a[data-element-name="Shop Household & Pet Essentials"]').click();
-        
-        //TODO:Need to scroll to element, not doing it by default
-        await page.locator('#category_Link>li:last-of-type>a').click();
-        
-        await page.waitForNavigation();
-        await page.locator('.product-card-container li.item.card__product:last-of-type a').click();
-        await page.waitForNavigation();
-        console.log(await page.evaluate(function(){
-            return $('#productTitle')[0].innerText;
-        }));
-    }
-
-    await navigate("Household & Pet Essentials");
+    page.goto(baseURL);
+    await page.waitForNavigation();
+    await setupJquerry(page);
+    let householdScraper = new CategoryScraper(page, "Household & Pet Essentials")
+    await householdScraper.scrape(10);
 })();
 
 async function setupJquerry(page){
@@ -38,15 +19,72 @@ async function setupJquerry(page){
 
 
 class CategoryScraper{
-    outData = new ReturnData();
-    constructor(category){
+    outData = [];
+    category;
+    page;
+
+    constructor(page, category){
         this.category = category;
+        this.page = page;
     }
 
-    async navigate(page){
+    async scrape(count){
+        await this.navigateToProductList(this.page);
+        for(let i=1; i<=count;i++){
+            //click into a product
+            await this.page.locator(`#productSection .product-container>li.card__product:nth-of-type(${i}) a:first-of-type`).click();
+            await this.page.waitForNavigation();
+            
+            //Pass control to product scraper
+            this.outData.push(await this.scrapeProductPage());
+            console.log(this.outData);
+            await this.page.goBack();
+        }
     }
 
-    async scrape(page){
+    async navigateToProductList(){
 
+        await this.page.locator('a.menu-trigger').click();
+        await this.page.locator('#menu-shop-products>a').click();
+        await this.page.locator(`#menu-shop-products li>a[data-element-name="${this.category}"]`).click();
+        await this.page.locator(`#menu-shop-products li>.show-next-lvl .right-links li>a[data-element-name="Shop ${this.category}"]`).click();
+        
+        //TODO:Need to scroll to element, not doing it by default
+        await this.page.locator('#category_Link>li:last-of-type>a').click();
+        await this.page.waitForNavigation();
+    }
+
+    async scrapeProductPage(){
+        return "Scraped page";
+    }
+}
+
+class ReturnDataTemplate {
+    
+    id = null;
+    productName = null;
+    listPrice = null;
+    description = null;
+    productDimensions = null;
+    imageURLs = null;
+    productUPC = null;
+    sourceURL = null;
+
+    clearEmtpy(){
+
+    }
+
+    getData(){
+        this.clearEmtpy();
+        return {
+            id,
+            productName,
+            listPrice,
+            description,
+            productDimensions,
+            imageURLs,
+            productUPC,
+            sourceURL
+        }
     }
 }
